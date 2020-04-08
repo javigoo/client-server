b97eab35cd5ec29959ad8a73d127809c9dea1899
javigoo <javigo_@outlook.com>
2020-04-08 02:18:44 +0200

    read_configuration()

--- src/server.c
+++ src/server.c
@@ -6,7 +6,7 @@
 
 /* Global variables */
 bool debug_flag = false;
-char configuration[] = "server.cfg"; /* A cuanto inicializo configuration[] ?*/
+char configuration_file[] = "server.cfg"; /* A cuanto inicializo configuration[] ?*/
 
 /* Structures */
 struct configuration_data
@@ -17,9 +17,9 @@ struct configuration_data
 };
 
 /* Functions */
-void parse_args(int argc,char *argv[]);
 void debug(char msg[]);
 void msg(char msg[]);
+void parse_args(int argc,char *argv[]);
 void read_configuration(struct configuration_data *configuration);
 
 
@@ -33,50 +33,27 @@ int main(int argc,char *argv[])
   parse_args(argc, argv);
   read_configuration(&configuration);
 
+  printf("\nConfiguration:\n");
+  printf("id = %s\n", configuration.id);
+  printf("udp_port = %i\n", configuration.udp_port);
+  printf("tcp_port = %i\n", configuration.tcp_port);
+
   return 1;
 }
 
 /* Auxiliary functions */
-void parse_args(int argc,char *argv[])
+void msg(char msg[])
 {
-  int arg;
-  for(arg = 0; arg < argc; arg++)
-  {
-    char const *opt = argv[arg];  /* char const ? */
-    if (opt[0] == '-')
-    {
-      switch (opt[1])
-      {
-        case 'd':
-          debug_flag = true;
-          break;
-        case 'c':
-          if (argc-arg == 2)
-          {
-            printf("Reading '%s' configuration file\n", argv[arg+1]);
-            strcpy(configuration, argv[arg+1]);
-            break;
-          }
-          fprintf(stderr, "Usage: %s [-c <configuration file>, default=server.cfg]\n", argv[0]);
-          exit(0);
-        case 'u':
-          if (argc-arg >= 2)
-          {
-            printf("Reading '%s' authorized file\n", argv[arg+1]);
-            msg("Select authorized file - Not Implemented");
-            break;
-          }
-          fprintf(stderr, "Usage: %s [u <authorized file>, default=bbdd_dev.dat]\n", argv[0]);
-          exit(0);
-        default:
-          fprintf(stderr, "Usage: %s [-d] "
-                          "[-c <configuration file>, default=server.cfg] "
-                          "[u <authorized file>, default=bbdd_dev.dat]\n", argv[0]);
-          exit(0);
-        }
-      }
-    }
-  }
+  time_t timer;
+  char buffer[9];
+  struct tm* tm_info;
+
+  timer = time(NULL);
+  tm_info = localtime(&timer);
+
+  strftime(buffer, 9, "%H:%M:%S", tm_info);
+  printf("%s: MSG -> %s\n", buffer, msg);
+}
 
 void debug(char msg[])
 {
@@ -94,20 +71,80 @@ void debug(char msg[])
   }
 }
 
-void msg(char msg[])
+void parse_args(int argc,char *argv[])
 {
-  time_t timer;
-  char buffer[9];
-  struct tm* tm_info;
-
-  timer = time(NULL);
-  tm_info = localtime(&timer);
-
-  strftime(buffer, 9, "%H:%M:%S", tm_info);
-  printf("%s: MSG -> %s\n", buffer, msg);
-}
+  /* Lo he hecho de esta forma porque no estoy seguro de poder utilizar <getopt.h> */
+  int arg;
+  for(arg = 0; arg < argc; arg++)
+  {
+    char const *opt = argv[arg];  /* char const ? */
+    if (opt[0] == '-')
+    {
+      switch (opt[1])
+      {
+        case 'd':
+          debug_flag = true;
+          break;
+        case 'c':
+          if (argc-arg >= 2)
+          {
+            strcpy(configuration_file, argv[arg+1]);
+            break;
+          }
+          fprintf(stderr, "Usage: %s [-c <configuration file>, default=server.cfg]\n", argv[0]);
+          exit(1);
+        case 'u':
+          if (argc-arg >= 2)
+          {
+            printf("Reading '%s' authorized file\n", argv[arg+1]);
+            msg("Select authorized file - Not Implemented");
+            break;
+          }
+          fprintf(stderr, "Usage: %s [u <authorized file>, default=bbdd_dev.dat]\n", argv[0]);
+          exit(1);
+        default:
+          fprintf(stderr, "Usage: %s [-d] "
+                          "[-c <configuration file>, default=server.cfg] "
+                          "[u <authorized file>, default=bbdd_dev.dat]\n", argv[0]);
+          exit(1);
+        }
+      }
+    }
+  }
 
 void read_configuration(struct configuration_data *configuration)
 {
-  debug("Reading configuration...");
+  char buffer[255];
+  FILE *fp;
+  if ((fp = fopen(configuration_file, "r")) == NULL)
+  {
+    fprintf(stderr, "Error! opening file %s\n", configuration_file);
+    exit(1);
+  }
+
+  while (fgets(buffer, 255, fp))
+  {
+    char *key_parameter = strtok(buffer, "= \n");
+    if (key_parameter)
+    {
+      char *parameter_value = strtok(NULL, "= \n");
+      if (parameter_value)
+      {
+        if(strcmp(key_parameter, "Id") == 0)
+        {
+          strcpy(configuration->id, parameter_value);
+        }
+        if (strcmp(key_parameter, "UDP-port") == 0)
+        {
+          configuration->udp_port = atoi(parameter_value);
+        }
+        if (strcmp(key_parameter, "TCP-port") == 0)
+        {
+          configuration->tcp_port = atoi(parameter_value);
+        }
+      }
+    }
+  }
+
+  fclose(fp);
 }
